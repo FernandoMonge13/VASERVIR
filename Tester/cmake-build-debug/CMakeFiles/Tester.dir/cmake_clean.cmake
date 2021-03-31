4f6dd86a11dcf6ba97ffa67181932025c5f1e059
Fernando <fercoche36964gmail.com>
2021-03-31 16:54:22 -0600

    TASTER

---
+++ Tester/cmake-build-debug/CMakeFiles/Tester.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tester_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/Tester_autogen.dir/ParseCache.txt"
+  "Tester_autogen"
+  "CMakeFiles/Tester.dir/Tester_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/Tester.dir/main.cpp.o"
+  "CMakeFiles/Tester.dir/mainwindow.cpp.o"
+  "Tester"
+  "Tester.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tester.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Tester/cmake-build-debug/CMakeFiles/Tester_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tester_autogen"
+  "Tester_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Tester_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
